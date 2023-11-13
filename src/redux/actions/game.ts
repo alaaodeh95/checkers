bc0a417fa20713997af5bbb97d882756f64ddc54
Alaa Odeh <v-alodeh@microsoft.com>
2023-11-13 21:20:30 +0200

    Fixing build

--- src/redux/actions/game.ts
+++ src/redux/actions/game.ts
@@ -4,7 +4,7 @@ import { ActionFn } from '../store/store';
 import { highlightMoves, makeMove } from './moves';
 import { difficultyToDepth, isSameCords } from '../../utils/game';
 import { Mode, Player, Node } from '../../types/gameTypes';
-import { alphaBetaSearch } from '../../ai';
+import { alphaBetaSearch } from '../../AI';
 
 export const handleSquareClick =
     (cords: [number, number], e?: React.MouseEvent<HTMLDivElement, MouseEvent>): ActionFn =>
